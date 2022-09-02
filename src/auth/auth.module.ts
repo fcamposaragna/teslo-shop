3dec7dc7793e4387087e6f53b9bd5201ec5e3910
fcamposaragna <fcamposaragna@gmail.com>
2022-09-02 16:47:37 -0300

    jwt strategy created

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,19 +6,19 @@ import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: ( configService:ConfigService)=>{
-        // console.log('JWT Secret', configService.get('JWT_SECRET'))
-        // console.log('JWT secret', process.env.JWT_SECRET)
         return {
           secret: configService.get('JWT_SECRET'),
           signOptions: {
@@ -27,13 +27,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         }
       }
     })
-    // JwtModule.register({
-    //   secret: process.env.JWT_SECRET,
-    //   signOptions: {
-    //     expiresIn: '2h'
-    //   }
-    // })
+
   ],
-  exports: [ TypeOrmModule ]
+  exports: [ TypeOrmModule, JwtStrategy, PassportModule, JwtModule ]
 })
 export class AuthModule {}
