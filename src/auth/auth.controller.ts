ec8f37f69005e5d3f4eb0c5be7c4a42dcbbacced
fcamposaragna <fcamposaragna@gmail.com>
2022-09-04 18:12:31 -0300

    jwt strategy created and custom decorators added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { GetUser } from './decorators/get-user.decorator';
+import { RawHeaders } from './decorators/rawHeaders.decorator';
 
 import { CreateUserDto, LoginUserDto } from './dto';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +19,21 @@ export class AuthController {
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testingPrivateRoute(
+    @GetUser() user: User,
+    @GetUser('email') userEmail:string,
+    @RawHeaders() rawHeaders : string[]
+  ){
+    return {
+      ok: true,
+      message: 'Hola mundo Private',
+      user,
+      userEmail,
+      rawHeaders
+    }
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,22 +4,24 @@ import { Repository } from 'typeorm';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { JwtPayload } from './interfaces/jwt-payload.interfaces';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
+
+    private readonly jwtService: JwtService
   ){}
 
 
   async create(createUserDto: CreateUserDto) {
     
     try{
-
       const { password, ...userData} = createUserDto;
-
       const user = this.userRepository.create({
         ...userData,
         password: bcrypt.hashSync( password, 10 )
@@ -27,7 +29,10 @@ export class AuthService {
       await this.userRepository.save(user)
       delete user.password;
 
-      return user;
+      return {
+        ...user,
+        token: this.getJwtToken({id: user.id})
+      };
     }catch(error){
       this.handleDBErrors(error)
     }
@@ -38,14 +43,24 @@ export class AuthService {
     const { password, email } = loginUserDto;
     const user = await this.userRepository.findOne({
       where: {email},
-      select: {email: true, password: true}
+      select: {email: true, password: true, id: true}
     });
 
     if(!user) throw new UnauthorizedException('Not valid credentials');
 
     if(!bcrypt.compareSync(password, user.password)) throw new UnauthorizedException('Not valid credentials');
 
-    return user;
+    return {
+      ...user,
+      token: this.getJwtToken({id: user.id})
+    };
+  }
+
+  private getJwtToken(payload:JwtPayload){
+
+    const token = this.jwtService.sign(payload);
+    return token;
+
   }
 
   private handleDBErrors(error:any):never{
