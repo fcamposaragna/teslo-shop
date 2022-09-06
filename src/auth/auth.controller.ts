fafe7aca5b91d045c83fb6de4fa840fc5bf44703
fcamposaragna <fcamposaragna@gmail.com>
2022-09-06 20:07:43 -0300

    Custom decorator @Auth created and implemented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { Auth } from './decorators/auth.decorator';
 import { GetUser } from './decorators/get-user.decorator';
 import { RawHeaders } from './decorators/rawHeaders.decorator';
+import { RoleProtected } from './decorators/role-protected.decorator';
 
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { ValidRoles } from './interfaces/valid-roles';
 
 @Controller('auth')
 export class AuthController {
@@ -34,6 +38,32 @@ export class AuthController {
       user,
       userEmail,
       rawHeaders
+    }
+  }
+  // @SetMetadata('roles', ['admin', 'super-user'])
+
+  @Get('private2')
+  @RoleProtected(ValidRoles.superUser)
+  @UseGuards(AuthGuard(), UserRoleGuard )
+  privateRoute2(
+    @GetUser() user: User
+  ){
+
+    return {
+      ok: true,
+      user
+    }
+  }
+
+  @Get('private3')
+  @Auth(ValidRoles.superUser, ValidRoles.admin)
+  privateRoute3(
+    @GetUser() user: User
+  ){
+
+    return {
+      ok: true,
+      user
     }
   }
 }
